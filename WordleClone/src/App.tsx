13373b1631de4c4cdbe3a46c6a20ec2dcbbbd19d
guromain <romain.guibert44@ynov.com>
2026-09-09 13:03:46 +0200

    [MODIF] Vérification a partir d'un tableau de lettre

--- WordleClone/src/App.tsx
+++ WordleClone/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className={styles.board} >
-        <Row values={['z','a','g','o','a']} check={true} answer={"troue"}/>
+        <Row values={['z','a','o','p','a']} check={true} answer={"troue"}/>
         <Row values={['t','r','u','o','a']} check={true} answer={"troue"}/>
         <Row values={['c','r','o','u','e']} check={true} answer={"troue"}/>
         <Row values={['t','r','o','u','e']} check={true} answer={"troue"}/>
